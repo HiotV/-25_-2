ec868d8a15f4ee21534e0d34fb906f5e4e0b16e9
HiotV <154884261+HiotV@users.noreply.github.com>
2025-12-11 21:11:26 +0500

    Лабораторная работа 2
    Для первого задания:
    Как оптимизировать код так, чтобы не использовать count == -1?

---
+++ Лабораторная работа 2/task2.py
@@ -0,0 +1,16 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+money_capital = 0
+count = 0
+
+while count < months:
+    money = -spend + salary
+    spend += spend * increase
+    count += 1
+    money_capital -= money
+
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+money_capital = round(money_capital)
+print("Подушка безопасности, чтобы протянуть", months, "месяцев без долгов:", money_capital)
